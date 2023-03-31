855e49234d25ae25a557a5cb16550d3849bc4fa0
matt <mathewandnana@gmail.com>
2023-03-30 23:52:49 -0500

    adding the question array and badge urls for there options

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+const licenseBadgeURLs = {
+  'MIT': 'https://img.shields.io/badge/License-MIT-yellow.svg',
+  'Apache 2.0': 'https://img.shields.io/badge/License-Apache%202.0-blue.svg',
+  'GPL 3.0': 'https://img.shields.io/badge/License-GPLv3-blue.svg',
+  'BSD 3': 'https://img.shields.io/badge/License-BSD%203--Clause-blue.svg',
+  'None': '',
+};
+
+
 function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
